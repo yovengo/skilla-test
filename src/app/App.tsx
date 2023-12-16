9e625cc107fd0196f0f216856e76b6bc81ade1b5
Daniil Ognevoy <yovengowo@gmail.com>
2023-12-16 10:43:23 +0300

    feat(app): assemble all in app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,19 @@
+import { CallsTable } from '../components/Calls';
+import { useSearchCalls } from '../hooks/useSearchCalls';
+import styles from './App.module.css';
+import { useSearchParams } from '../hooks/useSearchParams';
+import { Filters } from '../components/Filters/Filters/Filters';
+
 function App() {
+  const params = useSearchParams();
+  const { dateStart, dateEnd, inOut, sortBy, order } = params;
+  const { calls } = useSearchCalls({ dateStart, dateEnd, inOut, sortBy, order });
+
   return (
-      <section>
-        Skilla Test
-      </section>
+    <section className={styles.container}>
+      <Filters />
+      <CallsTable calls={calls?.results} />
+    </section>
   );
 }
 
